69d9c5aa2f48095217e3889ecb010c8557f5b468
David Ben-Gurion <rnithin@berkeley.edu>
2018-02-09 14:48:25 -0800

    fixed error

--- greeter.sol
+++ greeter.sol
@@ -11,7 +11,7 @@ contract greeter {
 	}
 
 	function fib(uint fib_number) public returns (uint) {
-	    uint[] myArray = new uint[](fib_number);
+	    uint[] memory myArray = new uint[](fib_number);
 	    myArray[0] = 1;
 	    myArray[1] = 1;
 	    for(uint i = 2;i <= fib_number; i++) {
