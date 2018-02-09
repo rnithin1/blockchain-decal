06cef1ce89a86fcb0c2bb86a700995c3a6d813c7
David Ben-Gurion <rnithin@berkeley.edu>
2018-02-09 12:57:07 -0800

    added xor

--- greeter.sol
+++ greeter.sol
@@ -10,17 +10,18 @@ contract greeter {
 		return greeting;
 	}
 
-	function fib(uint fib_number) public constant returns (uint) {
+	function fib(uint fib_number) public returns (uint) {
 	    uint[] myArray = new uint[](fib_number);
 	    myArray[0] = 1;
 	    myArray[1] = 1;
-	    
-	    for(i=2;i<=fib_number;i++) {
-	        
-            myArray[i] = myArray[i-1] + myArray[i-2];
-            
-        }
-        
-	    return myArray[fib_number]
+	    for(uint i = 2;i <= fib_number; i++) {
+            	myArray[i] = myArray[i - 1] + myArray[i - 2];
+	    }
+	    return myArray[fib_number];
 	}
+
+	function xor(uint a, uint b) public returns (uint) {
+		return (a ^ b);
+	}
+
 }
